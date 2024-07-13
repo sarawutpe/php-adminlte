7c02cb8c6c2103691e9b175643a9da08c0b42107
Sarawut Pe <85542134+sarawutpe@users.noreply.github.com>
2024-07-13 23:51:59 +0700

    .

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'https://php-adminlte.onrender.com';
 
 /*
 |--------------------------------------------------------------------------
@@ -184,7 +184,7 @@ $config['permitted_uri_chars'] = 'a-z 0-9~%.:_\-';
 | use segment based URLs.
 |
 */
-$config['enable_query_strings'] = TRUE;
+$config['enable_query_strings'] = FALSE;
 $config['controller_trigger'] = 'c';
 $config['function_trigger'] = 'm';
 $config['directory_trigger'] = 'd';
